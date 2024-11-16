d53df50cfb7835c8f5bf25a221ba48b8f8b94925
JonathanM1206 <jonathanmoesses@gmail.com>
2024-11-16 04:14:21 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+window.onload = function generador() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let whos = Math.floor(Math.random() * who.length);
+  let actions = Math.floor(Math.random() * action.length);
+  let whats = Math.floor(Math.random() * what.length);
+  let whens = Math.floor(Math.random() * when.length);
+
+  let excusas =
+    who[whos] + " " + action[actions] + " " + what[whats] + " " + when[whens];
+
+  document.getElementById("excusa").innerHTML = excusas;
 };
